071b83ff8341cd517f53a56eaebd44eb83bdf626
farr12345 <c4051400@newcastle.ac.uk>
2025-02-17 22:04:12 +0000

    Manufacturer class done

--- CarModel.java
+++ CarModel.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 // CarModel class represents an individual car model its attributes describing the car
 public class CarModel {
@@ -11,6 +10,11 @@ public class CarModel {
 
     // Constructor to initialize a CarModel object
    public CarModel(String name, double weight, double sales_price, int NSold, String type) {
+       // Validate car type
+       if (!type.equalsIgnoreCase("hatchback") && !type.equalsIgnoreCase("saloon") && !type.equalsIgnoreCase("estate")) {
+           throw new IllegalArgumentException("Invalid car type. Must be hatchback, saloon, or estate.");
+       }
+
        this.name = name;
        this.weight = weight;
        this.sales_price = sales_price;
@@ -41,52 +45,3 @@ public class CarModel {
 
 }
 
-// Manufacturer class represents a car manufacturer that inherits attributes from the Carmodel class CarModel
-public class Manfacturer extends CarModel {
-    //attributes for the name of the manufacturer and the list of their car models
-    private String Mname;
-    private List<List<String>> carModelsList = new ArrayList<>();
-
-    // Constructor for Manufacturer class
-    public Manfacturer(String Mname, String name, double weight, double sales_price, int NSold, String type) {
-        super(name, weight, sales_price, NSold, type);
-        this.Mname = Mname;
-    }
-
-    //getter methods for attributes
-    public String getMname() {
-        return Mname;
-    }
-
-    public List<List<String>> getCarModelsList() {
-        return carModelsList;
-    }
-
-    public void setCarModels(CarModel[] carModels) {
-        carModelsList.clear(); // Clear existing data before adding new models
-
-        for (CarModel model : carModels) {
-            List<String> attributes = new ArrayList<>();// List to store attributes of a single car model
-            // Add each attribute of the car model to the list
-            attributes.add("Name: " + model.getName());
-            attributes.add("Weight: " + model.getWeight());
-            attributes.add("Sales Price: " + model.getSales_price());
-            attributes.add("Number Sold: " + model.getNSold());
-            attributes.add("Type: " + model.getType());
-
-            carModelsList.add(attributes); // Add each car's attribute list to the main list
-        }
-    }
-
-
-
-    // Method to display manufacturer details and the list of car models
-    public void displayManufacturer() {
-        System.out.println("Manufacturer: " + getMname());
-        System.out.println("Car Models: ");
-        // Loop through the list of car models and print their attributes
-        for (int i = 0; i < carModelsList.size(); i++) {
-            System.out.println("Car Model " + (i + 1) + ": " + carModelsList.get(i));
-        }
-    }
-}
